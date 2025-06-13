7bccdeb0a0eb884571788a2c79386d9609cefe07
FeMaspe02 <femaspe02@gmail.com>
2025-06-13 17:25:28 +0200

    Aggiunto middleware

--- server.js
+++ server.js
@@ -6,6 +6,12 @@ const express = require('express');
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//Importa rotte
+
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
