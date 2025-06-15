37cec75f8d962d88b8558211fabd5a2048b234db
FeMaspe02 <femaspe02@gmail.com>
2025-06-15 16:45:32 +0200

    PATCH ordini

--- routes/orders.js
+++ routes/orders.js
@@ -127,4 +127,67 @@ router.get('/:id', authMiddleware(1), async (req, res) => {
     res.status(500).json({ message: 'Errore del server durante il recupero dell’ordine.' });
   }
 });
+
+
+// PATCH aggiorna stato ordine - protetta per cliente o admin
+
+// NB - l'aggiornamento puó essere fatto solo in certe condizioni:
+// - Cliente può aggiornare da "non pagato" a "in spedizione"
+// - Cliente può aggiornare da "in spedizione" a "concluso" o "in controversia"
+// - Admin può aggiornare da "in controversia" a "concluso"
+
+
+router.patch('/:id', authMiddleware(1), async (req, res) => {
+  const { id } = req.params;
+  const { stato: nuovoStato } = req.body;
+  const utente = req.user;
+
+  const statiValidi = ['non pagato', 'in spedizione', 'in controversia', 'concluso'];
+  if (!statiValidi.includes(nuovoStato)) {
+    return res.status(400).json({ message: 'Stato non valido.' });
+  }
+
+  try {
+    const { rows } = await pool.query(
+      `SELECT stato, cliente_id FROM ordini WHERE ordine_id = $1`,
+      [id]
+    );
+
+    if (rows.length === 0)
+      return res.status(404).json({ message: 'Ordine non trovato.' });
+
+    const ordine = rows[0];
+    const ruolo = utente.ruolo_id;
+    const isCliente = ordine.cliente_id === utente.id;
+
+    if (ruolo === 3) {
+      if (!(ordine.stato === 'in controversia' && nuovoStato === 'concluso')) {
+        return res.status(403).json({ message: 'Permesso negato per admin.' });
+      }
+    } else if (ruolo === 1 && isCliente) {
+      if (
+        !(
+          (ordine.stato === 'non pagato' && nuovoStato === 'in spedizione') ||
+          (ordine.stato === 'in spedizione' &&
+            (nuovoStato === 'concluso' || nuovoStato === 'in controversia'))
+        )
+      ) {
+        return res.status(403).json({ message: 'Permesso negato per cliente.' });
+      }
+    } else {
+      return res.status(403).json({ message: 'Permesso negato.' });
+    }
+
+    await pool.query(
+      `UPDATE ordini SET stato = $1 WHERE ordine_id = $2`,
+      [nuovoStato, id]
+    );
+
+    res.status(200).json({ message: 'Stato ordine aggiornato.' });
+  } catch (error) {
+    console.error('Errore aggiornamento stato ordine:', error);
+    res.status(500).json({ message: 'Errore del server.' });
+  }
+});
+
 module.exports = router;
