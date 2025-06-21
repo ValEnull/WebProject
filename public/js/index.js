78a34fbe556bc9c560454f7fbe37472615a6ee8f
Valerio Paolo Gazziero <valerio.gazziero@gmail.com>
2025-06-21 15:39:46 +0200

    modifica banner promozionale
    Rimosso "Sconto inaugurazione 15% su tutti i prodotti!"

--- public/js/index.js
+++ public/js/index.js
@@ -3,7 +3,6 @@
    ===================== */
 
 const promoMessages = [
-  "Sconto inaugurazione 15% su tutti i prodotti!",
   "Spedizione gratuita per ordini sopra €50!",
   "Collezione estiva disponibile ora!",
   "Lo stai cercando? Lo abbiamo!",
